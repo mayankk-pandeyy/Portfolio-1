73897d2eeff6a7043dcf7b797a35bb4d2f1ecafc
Mayank Pandey <immayank7781@gmail.com>
2024-06-02 23:58:48 +0530

    codes

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily : {
+        'Font-Jakarta': "Plus Jakarta Sans",
+        'preahvihear' : "Preahvihear",
+        'poppins' : "Poppins"
+      }
+    },
+  },
+  plugins: [],
+}
